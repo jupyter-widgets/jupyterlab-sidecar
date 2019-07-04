d357a288e1c28e94b04b61234ffeaa7b46acdd4e
Sylvain Corlay <sylvain.corlay@gmail.com>
2019-07-05 00:59:26 +0200

    Release 0.4.0

--- sidecar/_version.py
+++ sidecar/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 0)
+version_info = (0, 3, 0)
 __version__ = ".".join(map(str, version_info))
 
 # The version of the attribute spec that this package
