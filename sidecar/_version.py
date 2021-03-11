d70371ff756481b2663c2148445b642341827851
Jason Grout <jgrout6@bloomberg.net>
2021-03-10 17:20:15 -0800

    Bump versions for release
    0.5.1 python
    0.6.1 js
    1.1.0 data spec

--- sidecar/_version.py
+++ sidecar/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Project Jupyter.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 5, 0)
+version_info = (0, 5, 1)
 __version__ = ".".join(map(str, version_info))
 
 # The version of the attribute spec that this package
@@ -13,4 +13,4 @@ __version__ = ".".join(map(str, version_info))
 #
 # Update this value when attributes are added/removed from
 # your models, or serialized format changes.
-EXTENSION_SPEC_VERSION = '1.0.0'
+EXTENSION_SPEC_VERSION = '1.1.0'
--- src/version.ts
+++ src/version.ts
@@ -10,4 +10,4 @@
  * your models, or serialized format changes.
  */
 export
-const EXTENSION_SPEC_VERSION = '1.0.0';
+const EXTENSION_SPEC_VERSION = '1.1.0';
